9be0fcca163332f79f666d249bb6ea48bf87c7d6
flaport <floris.laporte@gmail.com>
2025-05-22 13:24:37 +0200

    jupyterlab plist

--- .scripts/install/jupyterlab.sh
+++ .scripts/install/jupyterlab.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-uv tool install jupyterhub \
+uv tool install jupyterlab \
   --with jupyter-bokeh \
-  --with jupyterlab \
   --with jupyterlab-code-formatter \
   --with jupyterlab-git \
   --with jupyterlab-hdf \
   --with jupyterlab-lsp \
   --with jupyterlab-myst \
   --with jupyterlab-myst \
+  --with pyright \
   --with jupyterlab-vim \
   --with myst-nb \
   --with nbstripout \
-  --with ipympl
+  --with ipympl \
+  --force
 
-ln -sf ~/Library/Jupyter/kernels ~/.local/share/uv/tools/jupyterhub/share/jupyter/kernels
+ln -sf ~/Library/Jupyter/kernels ~/.local/share/uv/tools/jupyterlab/share/jupyter/kernels
